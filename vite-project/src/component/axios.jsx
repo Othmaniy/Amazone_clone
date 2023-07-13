d40569b4596ce564a07b83b3e4fc696f738b54fa
Ousman <ousmanmuhammed137@gmail.com>
2023-07-13 15:39:32 +0200

    second commit

--- vite-project/src/component/axios.jsx
+++ vite-project/src/component/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 const instance =axios.create({
-    baseURL:'http://localhost:3000'
+    // baseURL:'http://localhost:3000'
+    baseURL:"https://yellow-underclothes.cyclic.app/"
 })
 
 export default instance;
